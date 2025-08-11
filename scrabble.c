7bb3a925ff51c1e6ec5754ed762cec14e823b9d9
dt3605 <daviteod2019@icloud.com>
2025-08-11 14:16:15 -0300

    scrabble.c

--- scrabble.c
+++ scrabble.c
@@ -1,53 +1,45 @@
-#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-// Pontuação das letras, índices 0 = A, 1 = B, ..., 25 = Z
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
                 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+int compute_score(char *word);
 
 int main(void)
 {
-    // Entrada das palavras
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    char word1[100];
+    char word2[100];
 
-    // Computa a pontuação
+    printf("Player 1: ");
+    fgets(word1, 100, stdin);
+    word1[strcspn(word1, "\n")] = '\0';
+
+    printf("Player 2: ");
+    fgets(word2, 100, stdin);
+    word2[strcspn(word2, "\n")] = '\0';
+
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // Decide o vencedor
     if (score1 > score2)
-    {
         printf("Player 1 wins!\n");
-    }
     else if (score2 > score1)
-    {
         printf("Player 2 wins!\n");
-    }
     else
-    {
         printf("Tie!\n");
-    }
+
+    return 0;
 }
 
-int compute_score(string word)
+int compute_score(char *word)
 {
     int sum = 0;
-
     for (int i = 0, n = strlen(word); i < n; i++)
     {
-        // Se for letra, calcula pontuação
         if (isalpha(word[i]))
-        {
-            // Converte para maiúscula para ter índice consistente
-            int idx = toupper(word[i]) - 'A';
-            sum += POINTS[idx];
-        }
+            sum += POINTS[toupper(word[i]) - 'A'];
     }
     return sum;
 }
-
